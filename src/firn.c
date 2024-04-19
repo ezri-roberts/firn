2fc05b67971536695ae7ded3bacc2b50ff100f1d
Ezri <ezriroberts@gmail.com>
2024-04-19 15:18:17 +0000

    Displayed file names get cut off when too long.

--- src/firn.c
+++ src/firn.c
@@ -69,8 +69,20 @@ void firn_update(firn *inst) {
 			fg = selected ? FG_BLACK : FG_WHITE;
 		}
 
-		unsigned long spacing = 40 - strlen(item->name);
-		_print(bk, fg, "%s%*luB\n", item->name, spacing, item->size);
+		if (strlen(item->name) > 55) { // Cutoff display of file names that are too long.
+
+			char name_sub[55];
+			memcpy(name_sub, &item->name[0], 54);
+			name_sub[54] = '\0';
+
+			unsigned long spacing = 60 - (strlen(name_sub) + 3);
+			_print(bk, fg, "%s...%*luB\n", name_sub, spacing, item->size);
+		} else {
+
+			unsigned long spacing = 60 - strlen(item->name);
+			_print(bk, fg, "%s%*luB\n", item->name, spacing, item->size);
+		}
+
 	}
 
 	firn_input(inst);
