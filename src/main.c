708464363a5b4af93c864261cfb358b31584ddda
Ezri <ezriroberts@gmail.com>
2024-04-19 21:14:54 +0000

    Now opens current working dir if no dir is provided.

--- src/main.c
+++ src/main.c
@@ -2,9 +2,10 @@
 
 int main(int argc, char *argv[]) {
 
-	(void)argc; (void)argv;
+	(void)argc;
 
-	firn inst = firn_new(argv[1]);
+	char *path = argv[1] ? argv[1] : ".";
+	firn inst = firn_new(path);
 
 	firn_update(&inst);
 	firn_destroy(&inst);
